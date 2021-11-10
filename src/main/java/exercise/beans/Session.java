ba1cce253af5e1b2fb209f49bb7ef9339b925822
Kcook5400 <70355813+Kcook5400@users.noreply.github.com>
2021-11-10 13:07:59 -0600

    Added index page as a test. DB is working locally

--- src/main/java/exercise/beans/Session.java
+++ src/main/java/exercise/beans/Session.java
@@ -13,7 +13,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
