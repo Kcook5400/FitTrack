3dd6adcd0deea0b7ec841e331b0d84e339b1c456
Noah Chung <78572452+noahchung757@users.noreply.github.com>
2021-11-15 19:27:32 -0600

    WebControllers and Repositories
    Added the WebControllers and Repositories for Users and Session classes.

---
+++ src/main/java/exercise/repository/SessionRepository.java
@@ -0,0 +1,9 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import exercise.beans.Session;
+
+public interface SessionRepository extends JpaRepository<Session, Long>{
+
+}
--- src/main/java/exercise/repository/UsersRepository.java
+++ src/main/java/exercise/repository/UsersRepository.java
@@ -2,13 +2,14 @@ package exercise.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import exercise.beans.Exercise;
+import exercise.beans.Users;
+
 
 /**
  * @author Noah Chung - nmchung
  * CIS175 - Fall 2021
  * Nov 15, 2021
  */
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
