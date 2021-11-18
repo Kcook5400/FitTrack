5a053d3c20a563308bdd1cbfb8f73eb3b41fab40
Kcook5400 <70355813+Kcook5400@users.noreply.github.com>
2021-11-18 08:38:13 -0600

    uncommented repo

--- src/main/java/exercise/repository/SessionRepository.java
+++ src/main/java/exercise/repository/SessionRepository.java
@@ -2,9 +2,8 @@ package exercise.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//import exercise.beans.Session;
-/*
-public interface SessionRepository extends JpaRepository<Session, Long>{
+import exercise.beans.WorkoutSession;
+
+public interface SessionRepository extends JpaRepository<WorkoutSession, Long>{
 
 }
-*/
